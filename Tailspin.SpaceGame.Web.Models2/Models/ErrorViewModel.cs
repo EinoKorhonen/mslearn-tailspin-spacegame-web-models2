73c01d0d0c4695a958d29f448cfdcafeb7a2910a
Timo Parviainen <timo_p65@yahoo.com>
2023-07-13 19:12:04 +0300

    Lisätty alkuperäisen projektin kamppeet

---
+++ Tailspin.SpaceGame.Web.Models2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TailSpin.SpaceGame.Web.Models2
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
